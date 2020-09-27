fc86fa699d3a65c4ee8e777a4640362f5caaacca
VishakaSubasith <subasith@gmail.com>
2020-09-27 20:09:35 +0530

    new batch UI done

---
+++ app/src/main/java/com/example/tcmsapp/calenderFM.java
@@ -0,0 +1,46 @@
+package com.example.tcmsapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+
+public class calenderFM extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_calender_f_m,container,false);
+       ImageView imageView = view.findViewById(R.id.std);
+
+       ImageView batches = view.findViewById(R.id.batches);
+       batches.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+               fragmentTransaction.replace(R.id.contrainer,new batchesfragment());
+               fragmentTransaction.commit();
+           }
+       });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.contrainer,new studentfragment());
+                fragmentTransaction.commit();
+
+            }
+        });
+
+       return view;
+    }
+}
